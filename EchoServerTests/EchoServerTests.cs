dcddcc2f00c923d7f16c7b76f3e9d8af0a3bf86e
FEAR-ops <ariktkacenko495@hmail.com>
2025-11-02 23:47:03 +0200

    переустановіл NUnit

--- EchoServerTests/EchoServerTests.cs
+++ EchoServerTests/EchoServerTests.cs
@@ -58,7 +58,7 @@ namespace EchoTspServer.Tests
         [Test]
         public void Constructor_SetsDependenciesProperly()
         {
-            NUnit.Framework.Assert.NotNull(_server);
+            Assert.IsNotNull(_server);   
         }
     }
 }
