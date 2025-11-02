3a9b5fe738bef5863a6e7bf382ef15196cc56760
FEAR-ops <ariktkacenko495@hmail.com>
2025-11-03 01:22:46 +0200

    додав пачку тестів для BinaryExtensions.cs

---
+++ NetSdrClientAppTests/BinaryExtensionsTests.cs
@@ -0,0 +1,116 @@
+﻿using NetSdrClientApp.Messages;
+
+namespace NetSdrClientAppTests
+{
+    public class NetSdrMessageHelperTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetControlItemMessageTest()
+        {
+            //Arrange
+            var type = NetSdrMessageHelper.MsgTypes.Ack;
+            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
+            int parametersLength = 7500;
+
+            //Act
+            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]);
+
+            var headerBytes = msg.Take(2);
+            var codeBytes = msg.Skip(2).Take(2);
+            var parametersBytes = msg.Skip(4);
+
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
+            var actualLength = num - ((int)actualType << 13);
+            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
+
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(actualCode, Is.EqualTo((short)code));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+        }
+
+        [Test]
+        public void GetDataItemMessageTest()
+        {
+            //Arrange
+            var type = NetSdrMessageHelper.MsgTypes.DataItem2;
+            int parametersLength = 7500;
+
+            //Act
+            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
+
+            var headerBytes = msg.Take(2);
+            var parametersBytes = msg.Skip(2);
+
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
+            var actualLength = num - ((int)actualType << 13);
+
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+        }
+
+        //TODO: add more NetSdrMessageHelper tests 
+        [Test]
+        public void GetSamples_ShouldReturnExpectedIntegers()
+        {
+            //Arrange
+            ushort sampleSize = 16; // 2 bytes per sample
+            byte[] body = { 0x01, 0x00, 0x02, 0x00 }; // 2 samples: 1, 2
+
+            //Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray();
+
+            //Assert
+            Assert.That(samples.Length, Is.EqualTo(2));
+            Assert.That(samples[0], Is.EqualTo(1));
+            Assert.That(samples[1], Is.EqualTo(2));
+        }
+
+        // ✅ BEGIN: Lab8 - Added for Sonar & Coverage
+        [Test]
+        public void GetSamples_ZeroLength_ReturnsEmptyArray()
+        {
+            //Arrange
+            ushort sampleSize = 16;
+            byte[] body = Array.Empty<byte>();
+
+            //Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray();
+
+            //Assert
+            Assert.That(samples.Length, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetSamples_WrongSampleSize_ReturnsPartialSamples()
+        {
+            //Arrange
+            ushort sampleSize = 16;
+            byte[] body = { 0x01, 0x00, 0x02 }; // 3 bytes → 1.5 sample
+
+            //Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToArray();
+
+            //Assert
+            Assert.That(samples.Length, Is.EqualTo(1)); // only full sample counted
+            Assert.That(samples[0], Is.EqualTo(1));
+        }
+
+
+    }
+}
